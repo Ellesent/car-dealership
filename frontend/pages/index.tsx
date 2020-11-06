737a65be19b86a05acbb1147085556533afcc195
ellesent <hypahflipperz@gmail.com>
2020-11-06 14:52:58 -0800

    adding typescript and bulma to project

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div>
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" />
+        </a>
+      </footer>
+      <a className="button is-primary">
+  Button
+</a>
+    </div>
+  )
+}
