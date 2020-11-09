b6d90d167a6d95ee55f275ed72308a8efdca82e4
ellesent <hypahflipperz@gmail.com>
2020-11-08 19:16:04 -0800

    added some styling to the car cards

--- frontend/components/car-card.tsx
+++ frontend/components/car-card.tsx
@@ -14,8 +14,15 @@ export const CarCard = (props: Props) => {
                 <img className={`${styles.img}`} src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
             </div>
             <div className="card-content">
-                <div className="content">
-                    {props.car.year} {props.car.make}
+                <div className={`${styles.cardContent} content is-flex is-flex-direction-column is-flex-wrap-wrap`}>
+                    <h1 className="has-text-primary"> {props.car.year} {props.car.make}</h1>
+                    <h5>Color: <strong>{props.car.color}</strong> </h5>
+                    <h5>Includes Sun Roof: <strong>{props.car.hasSunroof ? "Yes" : "No"}</strong></h5>
+                    <h5>Is Four Wheel Drive:<strong>{props.car.isFourWheelDrive ? "Yes" : "No"}</strong></h5>
+                    <h5>Has Low Mileage: <strong>{props.car.hasLowMiles ? "Yes" : "No"}</strong></h5>
+                    <h5>Window Type: <strong>{props.car.hasPowerWindows ? "Power" : "Manual"}</strong></h5>
+                    <h5>Includes Navigation: <strong>{props.car.hasNavigation ? "Yes" : "No"}</strong></h5>
+                    <h5>Seat Type: <strong>{props.car.hasHeatedSeats ? "Heated" : "Standard"}</strong></h5>
                     <br />
                 </div>
             </div>
